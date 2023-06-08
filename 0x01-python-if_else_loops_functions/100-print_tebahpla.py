98b421d77aecdc80c6eaf39f203c9e2b32d4d5f6
ishaqichaba <ichabaasiaka@gmail.com>
2023-06-07 20:30:51 -0700

    updated

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Yekini Oluwafemi
+# Author - Siaka Ichaba
 
 i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Yekini Oluwafemi
+# Author - Siaka Ichaba
 
 def remove_char_at(str, n):
     if n < 0:
